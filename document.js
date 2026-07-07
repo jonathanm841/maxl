60dac3ab0f742321c542ad0f44a2426faca90058
jonathanm841 <103659629+jonathanm841@users.noreply.github.com>
2026-07-07 16:02:36 +0100

    doc

--- document.js
+++ document.js
@@ -1,6 +1,13 @@
 import "./document.css";
 
+const PDF_TITLE = "The MaxL System";
+
 const downloadBtn = document.getElementById("download-pdf");
 if (downloadBtn) {
-  downloadBtn.addEventListener("click", () => window.print());
+  downloadBtn.addEventListener("click", () => {
+    const previousTitle = document.title;
+    document.title = PDF_TITLE;
+    window.print();
+    document.title = previousTitle;
+  });
 }
